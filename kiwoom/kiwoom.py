2175c0c55b7f4d80f4c49eae7b60d36b2f17a3d6
iljoomoon <iljoomoon@gmail.com>
2020-08-10 14:34:26 +0900

    Add login

---
+++ kiwoom/kiwoom.py
@@ -0,0 +1,37 @@
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+
+        print("Kiwoom 클래스입니다.")
+
+        ######## eventloop 모듈
+        self.login_next_loop = None
+        #############################
+
+        self.get_ocx_instance()
+        self.event_slots()
+
+        self.signal_login_commConnect()
+
+    def get_ocx_instance(self):
+        # 키움OpenAPI 프로그램 레지스트리 등록
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+
+    def login_slot(self, errCode):
+        print(errCode)
+
+        self.login_event_loop.exit()
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")
+
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
+
